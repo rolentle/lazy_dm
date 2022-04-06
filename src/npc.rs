149863db79cf4672976b6d943f9a1574321cc239
Rolen T. Le <rolentle@gmail.com>
2022-04-05 22:29:29 -0400

    Random NPC

--- src/npc.rs
+++ src/npc.rs
@@ -1,13 +1,21 @@
+use rand::thread_rng;
+use rand::seq::SliceRandom;
+
 #[derive(Debug)]
 pub struct NPC {
     pub behavior: String,
     pub ancestry: String,
 }
 
+const BEHAVIORS: [&str; 20] = ["Enthusiastic", "Flighty", "Shifty", "Optimistic", "Paranoid", "Well spoken", "Superior", "Haughty", "Pessimistic", "Suspicious", "Worried", "Greedy", "Brave", "Stern", "Sly", "Wise", "Reserved", "Cheery", "Opportunistic", "Soft spoken"];
+
+const ANCESTORIES: [&str; 20] = ["Human", "Elf", "Dwarf", "Halfling", "Orc", "Drow", "Tiefling", "Dragonborn", "Fey", "Goblin", "Construct", "Celestial", "Ghost", "Wizard's familiar", "Talking animal", "Avian", "Lizardfolk", "Catfolk", "Lycanthrope", "Artifact"];
+
 impl NPC {
     pub fn new() -> NPC {
-        let behavior = "Enthusiastic".to_string();
-        let ancestry = "Human".to_string();
+        let mut rng = thread_rng();
+        let behavior = BEHAVIORS.choose(&mut rng).unwrap().to_string();
+        let ancestry = ANCESTORIES.choose(&mut rng).unwrap().to_string();
         NPC { 
             behavior, 
             ancestry 
