d7fd4807bf22efa6e72112dac41dad4204351fff
Rolen T. Le <rolentle@gmail.com>
2022-04-09 21:57:13 -0400

    Working Yew

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
+use yew::prelude::*;
 mod npc;
 use npc::*;
-mod quest;
-use quest::*;
+// mod quest;
+// use quest::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    let npc = NPC::new();
+    let npc_name = format!("{} {}", npc.behavior, npc.ancestry);
+    // for _ in (0..10).into_iter() {
+    //     let quest = Quest::new();
+    //     println!("{} in a {}", quest.objective, quest.location);
+    // }
+    html! {
+    <div>
+        <h1>{ "Lazy DM's companion" }</h1>
+        <h2>{ npc_name }</h2>
+    </div>
+    }
+}
 
 fn main() {
-    let npc = NPC::new();
-    println!("{} {}", npc.behavior, npc.ancestry);
-    for _ in (0..10).into_iter() {
-        let quest = Quest::new();
-        println!("{} in a {}", quest.objective, quest.location);
-    }
+    yew::start_app::<App>();
 }
